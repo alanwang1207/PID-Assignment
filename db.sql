0cfcb834e524320a9f2ea15f5748f4b8303ec8d4
alanwang1207 <alan_wang@mail.chungyo.net>
2020-08-24 17:33:40 +0800

    detail表建立中

--- db.sql
+++ db.sql
@@ -27,6 +27,19 @@ CREATE TABLE IF NOT EXISTS `black_list` (
   FOREIGN KEY (`bid`) REFERENCES customer(`cid`)
   ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+CREATE TABLE `detail` (
+  `did` int NOT NULL auto_increment primary key,
+  `cid` int NOT NULL,
+  `prodname` varchar(20) NOT NULL,
+  `prodcount` int NOT NULL,
+  `cash` int,
+  `total` int DEFAULT 0,
+  FOREIGN KEY (`cid`) REFERENCES customer(`cid`),
+  FOREIGN KEY (`prodname`) REFERENCES prod(`prodname`),
+  FOREIGN KEY (`prodcount`) REFERENCES prod(`prodcount`),
+  FOREIGN KEY (`cash`) REFERENCES prod(`cash`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 
 INSERT INTO `user` (`uid`,`username`,`password`) VALUES
 (1,'aaa','111'),
---
+++ detail.php
@@ -0,0 +1,61 @@
+<?php
+require_once("config.php");
+$commandText = <<<SqlQuery
+select * from prod ;
+SqlQuery;
+
+$result = mysqli_query($link, $commandText);
+// var_dump($result);
+
+?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>PID - 購物網</title>
+</head>
+
+<body>
+
+    <div class="container">
+        <h2>購物系統 - 商品管理</h2>
+        <a href="../index.php" class="btn btn-outline-primary">回首頁</a>
+        <a href="./add.php?pid=<?= $row["pid"] ?>" class="btn btn-outline-success">新增</a>
+        <?php while ($row = mysqli_fetch_assoc($result)) { ?>
+            <span>
+                <a href="./edit.php?pid=<?= $row["pid"] ?>" class="btn btn-outline-info">修改</a>
+                <a href="./delete.php?pid=<?= $row["pid"] ?>" class="btn btn-outline-danger">刪除</a>
+            </span>
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th>商品編號</th>
+                        <th>商品名</th>
+                        <th>數量</th>
+                        <th>金額</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td><?= $row["pid"] ?></td>
+                        <td><?= $row["prodname"] ?></td>
+                        <td><?= $row["prodcount"] ?></td>
+                        <td><?= $row["cash"] ?></td>
+                    </tr>
+
+                </tbody>
+            </table>
+        <?php } ?>
+    </div>
+
+</body>
+
+</html>
